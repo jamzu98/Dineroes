4614c4e2d33fccf4e52c72b8788dac8123750f93
jamimakkonen <jami.j.makkonen@gmail.com>
2023-04-29 17:12:09 +0300

    Added a sign out button and functionality

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { auth } from '../../firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { signOut } from 'firebase/auth';
 
 const Navbar: React.FC = () => {
+  const [user] = useAuthState(auth);
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+
+  const handleSignOut = async () => {
+    try {
+      await signOut(auth);
+    } catch (error) {
+      console.error('Error signing out: ', error);
+    }
+  };
+
   return (
     <header className="bg-blue-500 text-white py-4">
-      <div className="container mx-auto px-4">
+      <div className="container mx-auto px-4 flex justify-between items-center">
         <h1 className="text-xl font-bold">Dineroes</h1>
+        {user && (
+          <div className="relative">
+            <button
+              className="text-white focus:outline-none hover:text-gray-100 font-bold"
+              onClick={() => setDropdownVisible(!dropdownVisible)}
+            >
+              {user.email}
+            </button>
+            {dropdownVisible && (
+              <div className="absolute right-0 mt-2 bg-white text-black shadow-md rounded py-2">
+                <button
+                  className="w-full text-left px-4 py-2 hover:bg-gray-200 focus:outline-none"
+                  onClick={handleSignOut}
+                >
+                  Sign out
+                </button>
+              </div>
+            )}
+          </div>
+        )}
       </div>
     </header>
   );
